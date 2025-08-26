f685fd4804ca7f97b60412a42b7b0c8f04025370
Gamson3 <gamson3@yahoo.com>
2025-08-26 17:08:16 +0200

    Update providers.tsx to use our new authContext

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -3,12 +3,17 @@
 import StoreProvider from "@/state/redux"; 
 import { Authenticator } from "@aws-amplify/ui-react";
 import Auth from "./(auth)/authProvider";
+import { AuthProvider } from "./(auth)/authContext";
 
 const Providers = ({ children } : { children: React.ReactNode }) => {
     return (
         <StoreProvider>
             <Authenticator.Provider>
-                <Auth>{children}</Auth>
+                <Auth>
+                    <AuthProvider>
+                        {children}
+                    </AuthProvider>
+                </Auth>
             </Authenticator.Provider>
         </StoreProvider>
     )
